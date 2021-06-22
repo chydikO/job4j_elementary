939f2443861291ef5e3ccd7c956308d9c2fec161
x220 <oleg.chudnovskiy.gmail.com>
2021-06-22 16:42:31 +0300

    5.7. Ипотека [#156315 #168633] refactoring

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,7 +3,6 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int monthly, double percent) {
         int year = 0;
-        double balance = amount;
         while (amount > 0) {
             amount = (int) ((amount + amount * percent / 100) - monthly);
             amount += amount * (percent / 100);
