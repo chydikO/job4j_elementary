28c86efad5a886ffcc1db6deb3df387c0e5e95a7
Chudnovskiy Oleg T510 <oleg.chudnovskiy@gmail.com>
2021-06-30 12:19:17 +0300

    6.4.2. Слово заканчивается на ... [#156317] - fish

---
+++ src/ru/job4j/array/EndsWith.java
@@ -0,0 +1,10 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        /* проверить. что массив word имеет последние элементы одинаковые с post */
+        
+        return result;
+    }
+}
